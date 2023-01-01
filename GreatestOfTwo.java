ce3668ef814715550fd026500a152a7beb5ff1bb
Gowrish_77 <103709921+Gowrish7@users.noreply.github.com>
2023-01-01 06:47:12 +0530

    Add files via upload

---
+++ GreatestOfTwo.java
@@ -0,0 +1,13 @@
+package namedprograms;
+import java.util.*;
+public class GreatestOfTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int a =sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println((a>b?a:b));
+	}
+
+}
