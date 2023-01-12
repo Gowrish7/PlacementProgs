8b2432fd7e7cb6dd8a99ba52919ce186870db614
Gowrish_77 <103709921+Gowrish7@users.noreply.github.com>
2023-01-12 14:31:15 +0530

    Add files via upload
    Program to divide the strings from the given word

---
+++ StringDivide.java
@@ -0,0 +1,16 @@
+package array;
+import java.util.*;
+public class StringDivide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String string = sc.nextLine();
+		System.out.println("Divided strings are");
+		  for (int i = 0; i < string.length(); i++) {
+	            System.out.print(string.charAt(i) + " ");
+	        }
+		
+	}
+
+}
